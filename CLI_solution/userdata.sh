07062288ec2ecc5965b85b4fc6d24ccf276eb82b
Gökhan YARDIMCI <109518586+gokhanwell@users.noreply.github.com>
2023-06-08 10:43:56 +0300

    Roman-Numerals-Converter

--- CLI_solution/userdata.sh
+++ CLI_solution/userdata.sh
@@ -4,7 +4,7 @@ yum install python3 -y
 pip3 install flask
 yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/gokhanwell/cli_deneme/main/templates/index.html
-wget -P templates https://raw.githubusercontent.com/gokhanwell/cli_deneme/main/templates//result.html
-wget https://raw.githubusercontent.com/gokhanwell/cli_deneme/main/app.py
+wget -P templates https://raw.githubusercontent.com/gokhanwell/Roman-Numerals-Converter/main/templates/index.html
+wget -P templates https://raw.githubusercontent.com/gokhanwell/Roman-Numerals-Converter/main/templates/result.html
+wget https://raw.githubusercontent.com/gokhanwell/Roman-Numerals-Converter/main/app.py
 python3 app.py
